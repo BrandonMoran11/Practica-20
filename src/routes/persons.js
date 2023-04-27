c10ace937e0ce8218d8cc08bf068376e269b1b4e
BrandonMoran11 <bmoran2@ucol.mx>
2023-04-26 23:54:04 -0600

    Primer commit de codigo

---
+++ src/routes/persons.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Person = require('../models/persons');
+
+router.get('/gente', async (req, res) => {
+    const Persons = await Person.find({});
+    res.render("index", ({ Persons }));
+});
+
+module.exports = router;
