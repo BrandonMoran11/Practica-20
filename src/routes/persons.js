6cb3c86b9b81419b67f264ccbe9242008515c179
BrandonMoran11 <bmoran2@ucol.mx>
2023-05-04 17:20:12 -0600

    documentacion

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+const express = require('express');//inyeccion de express
+const router = express.Router();//creacion del router
+const mongoose = require('mongoose');//inyeccion de mongoose
 
-const Person = require('../models/persons');
+const Person = require('../models/persons');//variable para guardar lo que exportamos en el archivo persons, carpeta models
 
-router.get('/gente', async (req, res) => {
+router.get('/gente', async (req, res) => {//router tipo get para renderizar la vista index en la ruta "/gente"
     const Persons = await Person.find({});
     res.render("index", ({ Persons }));
 });
-router.get('/agregarPeople', async (req,res) =>{
-    res.render('addPersons');
-})
-// router.post('/addPerson', (req,res))=> {
 
-// };
-    
 
-module.exports = router;
+
+
+module.exports = router;//exportamos el modulo
